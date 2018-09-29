c4df3c1488f7bd7ceacecc94581e1bbb6de74cb1
Brunomendoncapereia <brunodrose0607@gmail.com>
2018-09-29 10:13:41 -0300

    passo inicial ordenaçao de inserçao

--- agendaaed1.c
+++ agendaaed1.c
@@ -3,23 +3,41 @@
 
 void *pbuffer;
 
+typedef struct pessoa{
+	char nome[20];
+	int tel;
+
+} pessoa;
+
+void ordenarinsercao(int data[]);
+
 int main(){
-	
+	int data[100];
 	char *pes;
 	int *quant;
 	int *opcao;
 	int *i;
+	int *tmp,*k,*j;
 	
-	pbuffer=malloc(3*sizeof(int) + 20*sizeof(char));
+	pbuffer=malloc(6*sizeof(int) + 20*sizeof(char));
 	if(pbuffer==NULL){
 			printf("erro");
 			exit(1);
 		}
 	quant=pbuffer;
-	*quant=0;
 	opcao=quant+sizeof(int);	
 	i=opcao+sizeof(int);
+
+	tmp=i+sizeof(int);	
+	k=tmp+sizeof(int);
+	j=k+sizeof(int);
+	
+	
+	*quant=0;
 	*i=0;
+	*tmp=0;
+	*k=0;
+	*j=0;
 	
 	do{
 		printf("digite 1 para inserir\n");
@@ -32,11 +50,7 @@ int main(){
 			
 		if(*opcao==1){
 			
-			
-			
-			
-	
-		}
+			}
 			if(*opcao==2){
 			}
 				if(*opcao==3){
@@ -51,4 +65,23 @@ int main(){
 	free(pbuffer);
 	return 0;
 }
+
+
+void ordenarinsercao(int data[]){
+
+ int *tmp,*k,*j,*quant;
+
+for (*j=1; *j<*quant; (*j)++) { 
+		*k =*j-1;
+		*tmp = data[j]; 
+		while ( (*k>=0) && (*tmp < data[*k]) ) { 
+			data[*k+1] = data[*k];
+			 (*k)--; 
+		}
+
+		data[*k+1] = *tmp; 
+	}
+}
+
+
 
